eb884885b5a3f6dd0576786aeb3a51ff4f6e961e
Hanif Rodili <hanifrodili@gmail.com>
2023-07-20 21:42:18 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <div class="w-full flex justify-center flex-col gap-10 py-20 px-10">
+      <h-button :color="color" :shape="shape" :size="size" :disabled="disabled" :loading="loading">Hello World!</h-button>
+      <hr>
+      <div>
+        <div class="flex flex-col mb-4">
+          <label class="text-sm mb-1">Color</label>
+          <input type="color" class="border rounded w-full" v-model="color">
+        </div>
+        <div class="flex flex-col mb-4">
+          <label class="text-sm mb-1">Shape</label>
+          <select v-model="shape" class="p-2 border rounded">
+            <option value="rounded">Rounded</option>
+            <option value="box">Box</option>
+          </select>
+        </div>
+        <div class="flex flex-col mb-4">
+          <label class="text-sm mb-1">Size</label>
+          <select v-model="size" class="p-2 border rounded">
+            <option value="normal">Normal</option>
+            <option value="medium">Medium</option>
+            <option value="big">Big</option>
+          </select>
+        </div>
+        <div class="flex flex-col items-start mb-4">
+          <label class="text-sm mb-1">Disabled</label>
+          <input type="checkbox" v-model="disabled">
+        </div>
+        <div class="flex flex-col items-start">
+          <label class="text-sm mb-1">Loading</label>
+          <input type="checkbox" v-model="loading">
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import HButton from './components/HButton.vue'
+const color = ref('#009aa7')
+const shape = ref('rounded')
+const size = ref('normal')
+const disabled = ref(false)
+const loading = ref(false)
+</script>
+
+<style>
+</style>
---
+++ src/components/HButton.vue
@@ -0,0 +1,57 @@
+<template>
+  <button class="button button-custom bg-gradient" :class="`shape-${shape} size-${size}`" :style="{ backgroundColor: color }" :disabled="disabled" :loading="loading">
+    <slot v-if="!loading || disabled"></slot>
+    <div v-if="loading && !disabled">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="h-[40px] animate-spin"><title>loading</title><path d="M12,4V2A10,10 0 0,0 2,12H4A8,8 0 0,1 12,4Z" /></svg>
+    </div>
+  </button>
+</template>
+
+<script setup>
+import {ref} from 'vue'
+
+const props = defineProps(['color', 'shape', 'size', 'disabled', 'loading'])
+</script>
+
+<style>
+.button{
+  @apply min-h-[32px] min-w-[60px] flex justify-center items-center
+}
+.button-custom{
+  @apply rounded border-[2px] border-[#212529] text-[#212529] shadow-[0_5px_0px_rgba(33,37,41,1)] transition-none ease-linear relative
+}
+
+.button-custom:active{
+  @apply shadow-none top-[5px]
+}
+
+.button-custom:hover{
+  background: linear-gradient(180deg, rgba(255, 255, 255, 0.50), rgba(255, 255, 255, 0.3));
+}
+
+.button-custom:disabled{
+  @apply border-[#525252] text-[#525252] shadow-[#525252] !bg-slate-300 pointer-events-none
+}
+
+.shape-rounded{
+  @apply rounded-full
+}
+
+.shape-box{
+  @apply rounded-none
+}
+
+.bg-gradient{
+  background: linear-gradient(180deg, rgba(255, 255, 255, 0.30), rgba(255, 255, 255, 0));
+}
+
+.size-normal{
+  @apply min-h-[40px] min-w-[120px] w-fit text-[14px] px-2 py-1
+}
+.size-medium{
+  @apply min-h-[60px] min-w-[220px] w-fit text-[20px] px-4 py-2
+}
+.size-big{
+  @apply min-h-[80px] w-[400px] text-[30px] px-6 py-3
+}
+</style>
